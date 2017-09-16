bb3dcd8e2568657e504a73d0f9ad26fa8777b50f
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-16 23:18:46 +0200

    Add note impl

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,27 +3,45 @@ import Notes from './Notes';
 import React from 'react';
 import uuid from 'uuid';
 
-const notes: Note[] = [
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Do laundry'
+// tslint:disable-next-line:no-any
+class App extends React.Component<any, any> {
+  // tslint:disable-next-line:no-any
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ] as Note[]
+    };
+    this.addNote = this.addNote.bind(this);
   }
-];
+  addNote() {
+    this.setState({
+      notes: [
+        ...this.state.notes,
+        {
+          id: uuid.v4(),
+          task: 'New Task'
+        } as Note
+      ]
+    });
+  }
 
-class App extends React.Component {
   render() {
-    // tslint:disable:no-console
+    const { notes } = this.state;
     return (
       <div>
-        <button onClick={() => console.log('add note')}>+</button>
+        <button onClick={this.addNote}>+</button>
         <Notes notes={notes} />
       </div>
     );
-    // tslint:enable:no-console
   }
 }
 
