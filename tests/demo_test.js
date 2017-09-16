47ca668c822959cb23c554b7516f01b4cf31f60b
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-16 20:23:43 +0200

    :fireworks: migrate to TS/CreateReactApp

--- tests/demo_test.js
+++
@@ -1,7 +0,0 @@
-import * as assert from 'assert';
-
-describe('add', () => {
-  it('adds', () => {
-    assert.equal(1 + 1, 2);
-  });
-});
--- types/npm-install-webpack-plugin.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'npm-install-webpack-plugin';
