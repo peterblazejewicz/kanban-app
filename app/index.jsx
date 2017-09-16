368762737ec3918664287660664e6b99df7428d0
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2017-09-16 14:52:06 +0200

    Code formatting

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as Perf from 'react-addons-perf';
 
-if(process.env.NODE_ENV !== 'production') {
-  React.Perf = require('react-addons-perf');
+if (process.env.NODE_ENV !== 'production') {
+  Object.assign(React, {
+    Perf: Perf,
+  });
 }
 
-ReactDOM.render(
-  <div>Hello world</div>,
-  document.getElementById('app')
-);
+ReactDOM.render(<div>Hello world</div>, document.getElementById('app'));
--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,10 @@
 // Reference: http://karma-runner.github.io/0.13/config/configuration-file.html
-module.exports = function karmaConfig (config) {
+module.exports = function karmaConfig(config) {
   config.set({
     frameworks: [
       // Reference: https://github.com/karma-runner/karma-mocha
       // Set framework to mocha
-      'mocha'
+      'mocha',
     ],
 
     reporters: [
@@ -14,7 +14,7 @@ module.exports = function karmaConfig (config) {
 
       // Reference: https://github.com/karma-runner/karma-coverage
       // Output code coverage files
-      'coverage'
+      'coverage',
     ],
 
     files: [
@@ -23,7 +23,7 @@ module.exports = function karmaConfig (config) {
       'node_modules/phantomjs-polyfill/bind-polyfill.js',
 
       // Grab all files in the tests directory that contain _test.
-      'tests/**/*_test.*'
+      'tests/**/*_test.*',
     ],
 
     preprocessors: {
@@ -31,12 +31,12 @@ module.exports = function karmaConfig (config) {
       // Reference: https://github.com/webpack/karma-webpack
       // Convert files with webpack and load sourcemaps
       'tests/**/*_test.*': ['webpack', 'sourcemap'],
-      'app/**/*.*': 'coverage'
+      'app/**/*.*': 'coverage',
     },
 
     browsers: [
       // Run tests using PhantomJS
-      'PhantomJS'
+      'PhantomJS',
     ],
 
     singleRun: true,
@@ -44,22 +44,22 @@ module.exports = function karmaConfig (config) {
     // Configure code coverage reporter
     coverageReporter: {
       reporters: [
-          // generates ./coverage/lcov.info
-          {
-            type: 'lcovonly',
-            subdir: '.'
-          },
-          // generates ./coverage/coverage-final.json
-          {
-            type: 'json',
-            subdir: '.'
-          },
-          // generates ./coverage/index.html
-          {
-            type: 'html',
-            subdir: '.'
-          }
-      ]
+        // generates ./coverage/lcov.info
+        {
+          type: 'lcovonly',
+          subdir: '.',
+        },
+        // generates ./coverage/coverage-final.json
+        {
+          type: 'json',
+          subdir: '.',
+        },
+        // generates ./coverage/index.html
+        {
+          type: 'html',
+          subdir: '.',
+        },
+      ],
     },
 
     // Test webpack config
@@ -67,8 +67,7 @@ module.exports = function karmaConfig (config) {
 
     // Hide webpack build information from output
     webpackMiddleware: {
-      noInfo: true
-    }
+      noInfo: true,
+    },
   });
 };
-
